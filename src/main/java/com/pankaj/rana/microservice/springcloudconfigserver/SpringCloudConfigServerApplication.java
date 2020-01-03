28b43cf8dbf2a0ab5cef0675e9849674d760ae28
pankajrana737 <pankajrana737@gmail.com>
2020-01-03 16:46:21 +0530

    modified:   src/main/java/com/pankaj/rana/microservice/springcloudconfigserver/SpringCloudConfigServerApplication.java 	modified:   src/main/resources/application.properties

--- src/main/java/com/pankaj/rana/microservice/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ src/main/java/com/pankaj/rana/microservice/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.pankaj.rana.microservice.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
+// to enacle config otherwise ur app will not read configration from properties files
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
